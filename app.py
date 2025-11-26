57614a4e03bb7aebe86c73c9f2154818c659d5dd
priyaraj-10 <priyarajpillala1999@gmail.com>
2025-11-26 20:28:07 +0000

    not even a prototype

---
+++ app.py
@@ -0,0 +1,45 @@
+# from db.db import *
+from flask import Flask, render_template, url_for, request, flash, redirect, session
+from flask_wtf import CSRFProtect
+from flask_wtf.csrf import generate_csrf
+
+
+app = Flask(__name__)
+
+app.secret_key = 'your_secret_key'
+csrf = CSRFProtect(app) 
+
+@app.context_processor
+def inject_csrf_token():
+    return dict(csrf_token=generate_csrf())
+
+@app.route("/")
+def home ():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about ():
+    return render_template("about.html")
+
+
+@app.route("/login", strict_slashes=False)
+def loginform ():
+    return render_template("login.html")
+
+@app.route("/register", strict_slashes=False)
+def registeration():
+    return render_template("register.html")
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
